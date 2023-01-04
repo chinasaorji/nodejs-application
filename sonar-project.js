eb6d4fee1c3faf39d7ffc5231e2bad7fd61358d1
chinasaorji <118617265+chinasaorji@users.noreply.github.com>
2023-01-04 19:10:50 +0000

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://35.170.82.53:9000/',
+  serverUrl: 'http://3.239.1.61:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
