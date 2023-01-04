b084c78210fe1dafef853977d28f821149c8028f
chinasaorji <118617265+chinasaorji@users.noreply.github.com>
2023-01-04 18:42:06 +0000

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -5,7 +5,7 @@ sonarqubeScanner({
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJschinasaorji',
+	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
 	    'sonar.login': '27c6efcfaf22d8f9db8f8de7199db5d95f40db6b',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
